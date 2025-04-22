640efba6a438f25a271c3e1026b87620270c3999
rw <renwei12371@163.com>
2025-04-22 02:16:47 -0400

    Merge remote-tracking branch 'origin/gateway' into gateway
    # Conflicts:
    #	atm-user/target/classes/com/tss/atm/user/Mapper/UserMapper.class
    #	atm-user/target/classes/com/tss/atm/user/UserService.class
    #	atm-user/target/classes/com/tss/atm/user/controller/UserController.class
    #	atm-user/target/classes/com/tss/atm/user/entity/User.class

---
+++ atm-report/src/main/java/com/tss/atm/mapper/AttendanceReportMapper.java
@@ -0,0 +1,9 @@
+package com.tss.atm.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.tss.atm.entity.AttendanceReport;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface AttendanceReportMapper extends BaseMapper<AttendanceReport> {
+} 

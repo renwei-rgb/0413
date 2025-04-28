b773a86689e9e620810c1677e4ec6c367624144f
rw <wangkang883006@gmail.com>
2025-04-28 15:03:07 +0900

    优化

--- tss-atm-web-vue/src/main.js
+++ tss-atm-web-vue/src/main.js
@@ -1,6 +1,15 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import router from './router'
+import store from './store'
 
-createApp(App).mount('#app')
+Vue.use(ElementUI)
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
